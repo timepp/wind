83da1af6df96d24769ea6a56c916454e7ef29d53
tongjunhui <tongjunhui@live.cn>
2015-11-30 18:45:28 +0800

    add following functions to wind.string
    - splitHead: split the first part separated by 'sep'
    - splitTail: split all but the first part separated by 'sep'
    - stringFromCStringA: decode an ANSI string buffer to string
    - stringFromCStringW: decode an UNICODE string buffer to string
    - hexDump: return the hex representation of a buffer

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # wind
 small d wrapper functions, mainly for easier Windows development
+
+# wind.string
+- splitHead: split the first part separated by 'sep'
+- splitTail: split all but the first part separated by 'sep'
+- stringFromCStringA: decode an ANSI string buffer to string
+- stringFromCStringW: decode an UNICODE string buffer to string
+- hexDump: return the hex representation of a buffer
+
+
--- source/wind/string.d
+++ source/wind/string.d
@@ -1,8 +1,54 @@
 module wind.string;
 
 import std.string;
+import std.conv;
+import std.c.wcharh;
 
 string splitHead(string str, char sep)
 {
-    return str;
+    auto pos = str.indexOf(sep);
+    return pos == -1 ? str : str[0..pos];
 }
+
+string splitTail(string str, char sep, string defaultval)
+{
+    auto pos = str.indexOf(sep);
+    return pos == -1 ? defaultval : str[pos+1..$];
+}
+
+unittest
+{
+    assert(splitHead("var:val:vvv", ':') == "var");
+    assert(splitHead("variable", ':') == "variable");
+    assert(splitTail("var:val:vvv", ':', "xyz") == "val:vvv");
+    assert(splitTail("var", ':', "xyz") == "xyz");
+}
+
+string stringFromCStringA(void* buffer)
+{
+    immutable(char)* icstr = cast(immutable(char)*) buffer;
+    return to!string(icstr);
+}
+string stringFromCStringW(void* buffer)
+{
+    immutable(wchar)* icstr = cast(immutable(wchar)*) buffer;
+    wstring str = icstr[0..wcslen(icstr)];
+    return to!string(str);
+}
+
+string hexDump(void* buffer, uint len)
+{
+    string ret;
+    ubyte* p = cast(ubyte*)buffer;
+    for (uint i = 0; i < len; i++)
+    {
+        uint c = p[i];
+        uint x = c / 16;
+        ret ~= cast(char)((x >= 10)? x-10+'A' : x+'0');
+        uint y = c % 16;
+        ret ~= cast(char)((y >= 10)? y-10+'A' : y+'0');
+        ret ~= ' ';
+    }
+    return ret;
+}
+
